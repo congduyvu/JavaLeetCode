970de256d47256c5d69e828bbae50f16b36de895
congduyvu <congduyvu1997@gmail.com>
2024-08-11 13:40:32 +0700

    commit Roman Number exercise

--- src/exercise_effort/day1/Main.java
+++ src/exercise_effort/day1/Main.java
@@ -11,7 +11,13 @@ public class Main {
         int[] result1 = sumOrders.GetTwoSumOrder(nums1, target1);
         System.out.println("Output: [" + result1[0] + "," + result1[1] + "]");*/
 
-        PaLindrome paLindrome = new PaLindrome();
-        System.out.println(paLindrome.CheckPalindromeNumber(28282));;
+        //Exercise 2
+        /*PaLindrome paLindrome = new PaLindrome();
+        System.out.println(paLindrome.CheckPalindromeNumber(28282));;*/
+
+        //Exercise 3
+        RomanNumber romanNumber = new RomanNumber();
+        int result = romanNumber.convertToRomanNumber("XIV");
+        System.out.println(result);
     }
 }
--- src/exercise_effort/day1/RomanNumber.java
+++ src/exercise_effort/day1/RomanNumber.java
@@ -4,18 +4,31 @@ import java.util.HashMap;
 
 public class RomanNumber {
 
-    public String convertToRomanNumber(int number)
+    public int convertToRomanNumber(String number)
     {
-        HashMap<Integer, String> numberMap = new HashMap<>();
+        HashMap<Character, Integer> numberMap = new HashMap<>();
 
-        numberMap.put(1, "I");
-        numberMap.put(5, "V");
-        numberMap.put(10, "X");
-        numberMap.put(50, "L");
-        numberMap.put(100, "C");
-        numberMap.put(500, "D");
-        numberMap.put(1000, "M");
+        numberMap.put('I', 1);
+        numberMap.put('V', 5);
+        numberMap.put('X', 10);
+        numberMap.put('L', 50);
+        numberMap.put('C', 100);
+        numberMap.put('D', 500);
+        numberMap.put('M', 1000);
 
-        return "";
+        int total = 0;
+        int prevValue = 0;
+
+        for(int i = number.length()-1; i >= 0; i--){
+            char c = number.charAt(i);
+            int currentValue = numberMap.get(c);
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return total;
     }
 }
